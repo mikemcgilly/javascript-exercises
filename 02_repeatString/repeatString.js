7afc4290ddce3b6f589e1ca7e6602f3220659bb1
mikemcgilly <mikemcgilly@gmail.com>
2022-12-08 09:17:29 -0600

    complete 02 repeatstring testing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = '';
+    for(let i = 0; i < num; i++) {
+        newString += string;
+    }
+    if(num < 0) {
+        return 'ERROR';
+    }else if(num > 0) {
+        return newString;
+    }else if(num == 0) {
+        return '';
+    }
 };
 
 // Do not edit below this line
