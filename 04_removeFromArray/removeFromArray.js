83b46227a0ceca3e0ef549e10b050602bad2a92f
mikemcgilly <mikemcgilly@gmail.com>
2022-12-08 12:36:34 -0600

    finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+//created argument "...theArgs" because there will be a variety of inputs potentially, makes into array
+const removeFromArray = function(arrayInput, ...theArgs) {
+    //loop thru argument array first. 
+    for(let args of theArgs) {
+        //for each argument loop through arrayInput and remove matches
+        for(let i = 0; i < arrayInput.length; i++) {
+            if(arrayInput[i] === args) {
+                arrayInput.splice(i,1);
+            }
+        }   
+    }
+   return arrayInput;
 };
 
+// console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
