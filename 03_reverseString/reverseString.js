48751196a70e4653dada208da89aa479fba215af
mikemcgilly <mikemcgilly@gmail.com>
2022-12-08 09:35:35 -0600

    finish testing 03_reversestring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    const reverseArray = [];
+    for(let i = inputString.length; i >= 0; i--) {
+        reverseArray.push(inputString[i]);
+    }
+    return reverseArray.join('');
 };
 
 // Do not edit below this line
