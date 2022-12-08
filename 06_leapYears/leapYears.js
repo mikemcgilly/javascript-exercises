0f2b55f7b2c8fb9e2ae49af220006d796a9e9df5
mikemcgilly <mikemcgilly@gmail.com>
2022-12-08 14:54:39 -0600

    finish 06 leapyears easy

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(yearToCheck) {
+    let answer = false;
 
+    //div by 400 and 100
+    if((yearToCheck % 400 == 0) && (yearToCheck % 100 == 0)) {
+        answer = true;
+    }else if(yearToCheck % 100 == 0) { //div by 100
+        answer = false;
+    }else if(yearToCheck % 4 == 0) { //div by 4
+        answer = true;
+    }
+
+    return answer;
 };
 
 // Do not edit below this line
