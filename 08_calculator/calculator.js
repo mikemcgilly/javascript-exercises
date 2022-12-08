051e4ef38b8b7248e9342dc29e93df63aa8cfef4
mikemcgilly <mikemcgilly@gmail.com>
2022-12-08 16:25:54 -0600

    started 08_calculator not finished yet

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,37 @@
 const add = function() {
-	
+  let runningTotal = 0;
+  for(let i = 0; i < arguments.length; i++) {
+    runningTotal += arguments[i];
+  }
+  return runningTotal;
 };
 
 const subtract = function() {
+  let runningDifference = 0;
+  for(let i = 0; i < arguments.length; i++) {
+    //if the first iteration set running difference to first arg
+    if(i == 0){
+      runningDifference = arguments[i];
+    }else if(i > 0) {
+      runningDifference -= arguments[i];
+    }
+  }
+  return runningDifference;
 	
 };
 
 const sum = function() {
-	
+  let runningTotal = 0;
+  console.log("first element type: " + typeof arguments[0]);
+  console.log("second element type : " + typeof arguments[1]);
+  if(arguments[0] == '') {
+    return 0;
+  }else {
+    for(i = 0; i < arguments.length; i++) {
+      runningTotal += arguments[i];
+    }
+    return parseInt(runningTotal);
+  }
 };
 
 const multiply = function() {
