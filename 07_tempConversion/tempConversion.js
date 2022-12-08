1f5178ab535ca5b8d60a8d425b92f4050324decb
mikemcgilly <mikemcgilly@gmail.com>
2022-12-08 15:16:24 -0600

    finish 07_tempConversions easy

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureF) {
+  let fToC = (temperatureF - 32) * (5/9);// from C to F: (temperatureF * (9/5) + 32);
+  fToC = Math.round(fToC * 10) / 10;
+  console.log("F to C: " + fToC);
+  return fToC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureC) {
+  let cToF = (temperatureC * (9/5) + 32);
+  cToF = Math.round(cToF * 10) / 10;
+  console.log("C to F: " + cToF);
+  return cToF;
 };
 
 // Do not edit below this line
